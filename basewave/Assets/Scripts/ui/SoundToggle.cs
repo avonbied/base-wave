477debc02897ef3202eb30e926598fd69c96aac8
Lex von Biedenfeld <lexvonbon@users.noreply.github.com>
2020-02-02 11:52:12 -0600

    Update Start Scene

--- basewave/Assets/Scripts/ui/SoundToggle.cs
+++ basewave/Assets/Scripts/ui/SoundToggle.cs
@@ -7,10 +7,10 @@ public class SoundToggle : MonoBehaviour {
     public Sprite ToggleIcon;
     public Toggle toggle;
     public void ToggleState() {
-        // ToggleIcon =  Resources.Load<Sprite>((toggle.isOn) ? "speaker-on" : "speaker-off");
-        // toggle.GetComponent<Background>().image = ToggleIcon;
+        ToggleIcon = Resources.Load<Sprite>((toggle.value) ? "speaker-on" : "speaker-off");
+        //toggle.GetComponent<Background>().image = ToggleIcon;
         // if (toggle.isOn) {
-        //     Debug.Log("Toggle disabled!");
+            Debug.Log("Toggle disabled! " + toggle.value);
         // } else {
         //     Debug.Log("Toggle enabled!");
         // }
