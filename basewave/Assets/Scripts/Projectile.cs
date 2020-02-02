7d4e2c06741004f567cccd149fb2285d9171d63c
Matthew Sitton <matthewsitton@gmail.com>
2020-02-02 13:21:44 -0600

    Fix projectile collisions

--- basewave/Assets/Scripts/Projectile.cs
+++ basewave/Assets/Scripts/Projectile.cs
@@ -15,12 +15,14 @@ public class Projectile : MonoBehaviour
     private List<Collider2D> Colliders = new List<Collider2D>();
     public bool Live = true;
     public TrailRenderer MyTrailRenderer;
+
     private void Awake()
     {
         poolLink = GetComponent<PooledObject>();
         rigidBody = GetComponent<Rigidbody2D>();
         projectileCollider = GetComponent<Collider2D>();
-        if (MyTrailRenderer) MyTrailRenderer.Clear();
+        if (MyTrailRenderer)
+            MyTrailRenderer.Clear();
     }
 
     public virtual void Reset(Vector2 position, Quaternion rotation, Vector2 velocity, float flightTime)
@@ -31,7 +33,8 @@ public class Projectile : MonoBehaviour
         rigidBody.velocity = velocity;
         rigidBody.angularVelocity = 0.0f;
         transform.parent = null;
-        if (MyTrailRenderer) MyTrailRenderer.Clear();
+        if (MyTrailRenderer)
+            MyTrailRenderer.Clear();
         Live = true;
     }
 
@@ -41,46 +44,34 @@ public class Projectile : MonoBehaviour
         if ((flightTime <= 0.0f) || (!Live))
         {
             Live = false;
-            ParticleManager.EmitAt(ParticleManager.TheParticleManager.PlasmaImpact,this.transform.position);
+            ParticleManager.EmitAt(ParticleManager.TheParticleManager.PlasmaImpact, this.transform.position);
             poolLink.Return();
         }
 
-        
-        if (false)//Marcus: "There seems to be some performance overhead correlating to projectiles"
+        projectileCollider.OverlapCollider(ContactFilter, Colliders);
+        if (Colliders.Count > 0)
         {
-            projectileCollider.OverlapCollider(ContactFilter, Colliders);
-            if (Colliders.Count > 0)
-            {
-                Colliders[0].GetComponent<Entity>().Hit(Damage);
-                poolLink.Return();
-            }
+            Colliders[0].GetComponent<Entity>().Hit(Damage);
+            poolLink.Return();
         }
-        
+    }
 
-    }
-    Entity hitentity;
-    private void OnTriggerEnter2D(Collider2D collision) //Marcus: "Projectiles had triggers in the first place. Might as well use them."
-    {
-        if (!Live) return;
-        if (collision != null)//(Colliders.Count > 0)
-        {
-            hitentity = collision.GetComponent<Entity>();
-            if ((hitentity != null)) {
-                if (this.Friendly != hitentity.Friendly)
-                {
-                    ParticleManager.EmitAt(ParticleManager.TheParticleManager.PlasmaImpact, this.transform.position);
-                    hitentity.Hit(Damage);
-                    Live = false;
-                    poolLink.Return();
-                } else
-                {
-                    //Hit ally
-                }
-            } else
-            {
-                //Live = false;
-                //poolLink.Return();
-            }
-        }
-    }
+    // private void OnTriggerEnter2D(Collider2D collision) //Marcus: "Projectiles had triggers in the first place. Might as well use them."
+    // {
+    //     if (!Live && collision == null)
+    //         return;
+
+    //     Entity hitentity = collision.GetComponent<Entity>();
+
+    //     if (hitentity == null)
+    //         return;
+
+    //     if (this.Friendly != hitentity.Friendly)
+    //     {
+    //         ParticleManager.EmitAt(ParticleManager.TheParticleManager.PlasmaImpact, this.transform.position);
+    //         hitentity.Hit(Damage);
+    //         Live = false;
+    //         poolLink.Return();
+    //     }
+    // }
 }
