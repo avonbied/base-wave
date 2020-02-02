03e6a58dd15115cb59aff499e97c25e14232d9cd
Lex von Biedenfeld <lexvonbon@users.noreply.github.com>
2020-02-02 08:27:00 -0600

    Update Game Instance

--- basewave/Assets/Scripts/Game.cs
+++ basewave/Assets/Scripts/Game.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 public class Game : MonoBehaviour {
 
     private Controller Base;
+    private PoolManager ObjPool;
 
     
     public void Awake() {
-
+        
     }
     public void FixedUpdate() {
 
