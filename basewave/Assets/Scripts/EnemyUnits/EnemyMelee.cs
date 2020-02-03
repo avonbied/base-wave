4a7515721fa873c72e5bcc3c466438564ace66c1
Matthew Sitton <matthewsitton@gmail.com>
2020-02-02 20:39:27 -0600

    Actuially fix Melee unit hp ramping

--- basewave/Assets/Scripts/EnemyUnits/EnemyMelee.cs
+++ basewave/Assets/Scripts/EnemyUnits/EnemyMelee.cs
@@ -8,6 +8,7 @@ public class EnemyMelee : EntityMelee
 
     private void Start()
     {
+        base.Start();
         TargetFilter = new ContactFilter2D() { useLayerMask = true, layerMask = LayerMask.GetMask("Friendly") };
         Friendly = false;
     }
